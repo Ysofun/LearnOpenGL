d19052af24377b265d2d583b755973b5ba1255aa
Ysofun <787839825@qq.com>
2022-04-13 18:52:28 +0800

    添加项目文件。

---
+++ LearnOpenGL/src/Application.cpp
@@ -0,0 +1,91 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "Renderer.h"
+
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "shader.h"
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(5);
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error!" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	{
+		float positions[] = {
+			-0.5f, -0.5f,
+			 0.5f, -0.5f,
+			 0.5f,  0.5f,
+			-0.5f,  0.5f
+		};
+
+		unsigned int indices[] = {
+			0, 1, 2,
+			2, 3, 0
+		};
+
+		VertexArray va;
+		VertexBuffer vb(positions, 8 * sizeof(float));
+
+		VertexBufferLayout layout;
+		layout.Push<float>(2);
+		va.AddBuffer(vb, layout);
+
+		IndexBuffer ib(indices, 6);
+
+		Shader shader("res/shaders/Basic.shader");
+
+		float r = 0.8f;
+		float increment = 0.05f;
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* Render here */
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			va.Bind();
+			ib.Bind();
+			shader.Bind();
+			shader.SetUniform4f("u_Color", r, 0.5f, 0.8f, 1.0f);
+
+			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+			if (r > 1.0f) increment = -0.05f;
+			else if (r < 0.0f) increment = 0.05f;
+			r += increment;
+
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
+
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
+	}
+
+	glfwTerminate();
+	return 0;
+}
